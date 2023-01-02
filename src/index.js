80f7a840ffa0b5d3f60d420e1d374f00eba98115
Kiran Rathor <kiran@thoughtwin.com>
2023-01-02 11:11:08 +0530

    reacr map

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App2 from './map/App2'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App2 />
     <App itemsPerPage={4} />
   </React.StrictMode>
 );
